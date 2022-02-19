30f9fc7f8514c2cb861ec11b127c684300c3df7b
Alessandro Lotta <alessandro.lotta@studenti.unipd.it>
2022-02-19 17:07:55 +0100

    added the circular indicators for the skills

--- my_cv_project/lib/screens/skills/skills_screen.dart
+++ my_cv_project/lib/screens/skills/skills_screen.dart
@@ -87,10 +87,13 @@ class SkillsScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Text("Dart"),
-                            Text("Python"),
+                            CircularSkillIndicator(
+                              label: "Dart",
+                              value: 0.5,
+                            ),
+                            CircularSkillIndicator(label: "Python", value: 0.6),
                           ],
                         ),
                         SizedBox(
@@ -109,10 +112,10 @@ class SkillsScreen extends StatelessWidget {
                           height: defaultPadding,
                         ),
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Text("PostgreSQL"),
-                            Text("Java"),
+                            CircularSkillIndicator(label: "SQL", value: 0.6),
+                            CircularSkillIndicator(label: "Java", value: 0.7),
                           ],
                         ),
                       ],
@@ -126,6 +129,49 @@ class SkillsScreen extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class CircularSkillIndicator extends StatelessWidget {
+  const CircularSkillIndicator({
+    Key? key,
+    required this.label,
+    required this.value,
+  }) : super(key: key);
+
+  final String label;
+  final double value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 75,
+          width: 75,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              CircularProgressIndicator(
+                value: value,
+                color: primaryColor,
+              ),
+              Center(
+                child: Text(
+                  (value * 100).toString() + "%",
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: defaultPadding / 2),
+        Text(
+          label,
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+      ],
     );
   }
 }
