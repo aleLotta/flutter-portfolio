ec7e2649686f8ec1dedc558af7e37c67190a2133
Alessandro Lotta <alessandro.lotta@studenti.unipd.it>
2022-02-20 21:57:12 +0100

    added work screen and section

--- my_cv_project/lib/main.dart
+++ my_cv_project/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_cv_project/components/coded_name.dart';
+import 'package:my_cv_project/components/navigation_banner.dart';
 import 'package:my_cv_project/constants.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:my_cv_project/screens/home/home_screen.dart';
 import 'package:my_cv_project/screens/skills/skills_screen.dart';
+import 'package:my_cv_project/screens/works/work_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,20 +17,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "My Portfolio",
-      theme: ThemeData.dark().copyWith(
-        primaryColor: primaryColor,
-        scaffoldBackgroundColor: bgColor,
-        canvasColor: bgColor,
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-            .apply(bodyColor: Colors.white)
-            .copyWith(
-              bodyText1: TextStyle(color: bodyTextColor),
-              bodyText2: TextStyle(color: bodyTextColor),
-            ),
-      ),
-      home: SkillsScreen(), //HomeScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "My Portfolio",
+        theme: ThemeData.dark().copyWith(
+          primaryColor: primaryColor,
+          scaffoldBackgroundColor: bgColor,
+          canvasColor: bgColor,
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+              .apply(bodyColor: Colors.white)
+              .copyWith(
+                bodyText1: TextStyle(color: bodyTextColor),
+                bodyText2: TextStyle(color: bodyTextColor),
+              ),
+        ),
+        home: WorksScreen() //SkillsScreen(), //HomeScreen(),
+        );
   }
 }
---
+++ my_cv_project/lib/screens/works/work_screen.dart
@@ -0,0 +1,108 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:my_cv_project/components/coded_name.dart';
+import 'package:my_cv_project/components/navigation_banner.dart';
+
+import '../../constants.dart';
+
+class WorksScreen extends StatelessWidget {
+  const WorksScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Column(
+        children: [
+          CodedName(),
+          Expanded(
+            flex: 4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Still working on my first projects",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6!
+                          .copyWith(color: primaryColor),
+                    ),
+                    Container(
+                      width: 50,
+                      child: AnimatedTextKit(
+                        animatedTexts: [
+                          TyperAnimatedText(
+                            "",
+                            textStyle: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(color: primaryColor),
+                          ),
+                          TyperAnimatedText(
+                            ".",
+                            textStyle: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(color: primaryColor),
+                          ),
+                          TyperAnimatedText(
+                            "..",
+                            textStyle: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(color: primaryColor),
+                          ),
+                          TyperAnimatedText(
+                            "...",
+                            textStyle: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(color: primaryColor),
+                          )
+                        ],
+                        totalRepeatCount: 4,
+                        pause: Duration(milliseconds: 500),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: defaultPadding * 2,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "If you have any, please contact me :)",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(
+                      width: defaultPadding / 2,
+                    ),
+                    /*Container(
+                      width: 50,
+                      height: 50,
+                      child: AnimatedTextKit(animatedTexts: [
+                        FlickerAnimatedText(":)",
+                            textStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold))
+                      ]),
+                    )*/
+                  ],
+                ),
+              ],
+            ),
+          ),
+          NavigationBanner(),
+        ],
+      ),
+    );
+  }
+}
